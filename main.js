a5136f967783a55f244fa130e2fbe63bdf0c9f9e
Denis <gundenis1@gmail>
2024-06-23 16:02:35 +0300

    ds

--- main.js
+++ main.js
@@ -66,3 +66,31 @@ function toggleDropdown() {
   });
 
   document.documentElement.style.overflow = 'hidden';
+
+
+  // Определение операционной системы (iOS или Android)
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if (/android/i.test(userAgent)) {
+      return "Android";
+  }
+
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return "iOS";
+  }
+
+  return "unknown";
+}
+
+// Применение соответствующего паддинга в зависимости от операционной системы
+document.addEventListener("DOMContentLoaded", function() {
+  var footer = document.querySelector(".footer");
+  var os = getMobileOperatingSystem();
+
+  if (os === "iOS") {
+      footer.style.padding = "10px 0"; // паддинг для iOS
+  } else {
+      footer.style.padding = "20px 0"; // паддинг для Android
+  }
+});
