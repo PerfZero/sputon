3ac99f23364363fd493066a0a9da9e61cdb87faf
Denis <gundenis1@gmail>
2024-07-12 16:58:32 +0300

    ss

--- script.js
+++ script.js
@@ -41,3 +41,33 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+  // Определение операционной системы (iOS или Android)
+  function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+  
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+  
+    return "unknown";
+  }
+  
+  // Применение соответствующего паддинга в зависимости от операционной системы
+  document.addEventListener("DOMContentLoaded", function() {
+    var footer = document.querySelector(".footer");
+    var os = getMobileOperatingSystem();
+  
+    if (os === "iOS") {
+        footer.style.padding = "20px 0"; // паддинг для iOS
+    } else {
+        footer.style.padding = "10px 0"; // паддинг для Android
+    }
+  });
+  
+  
