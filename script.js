b0c70524677dc163682f801cf60dc7625846a39c
Denis <gundenis1@gmail>
2024-07-15 11:26:07 +0300

    test

--- script.js
+++ script.js
@@ -313,3 +313,9 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+const overflow = 100
+document.body.style.overflowY = 'hidden'
+document.body.style.marginTop = `${overflow}px`
+document.body.style.height = window.innerHeight + overflow + "px"
+document.body.style.paddingBottom = `${overflow}px`
+window.scrollTo(0, overflow)
