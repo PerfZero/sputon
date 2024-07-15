d3e4189a1550328406b70b4b64e48d6253d26247
Denis <gundenis1@gmail>
2024-07-15 10:38:00 +0300

    ds

--- script.js
+++ script.js
@@ -313,9 +313,3 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-const overflow = 100
-document.body.style.marginTop = `${overflow}px`
-document.body.style.height = window.innerHeight + overflow + "px"
-document.body.style.paddingBottom = `${overflow}px`
-window.scrollTo(0, overflow)
-
